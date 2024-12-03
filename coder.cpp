254213a319bdf218cfb5cb077591b8fd469fb034
Ivan <115657086+Ivanatom90@users.noreply.github.com>
2024-12-03 23:30:38 +0300

    Add files via upload

---
+++ coder.cpp
@@ -0,0 +1,25 @@
+#include "coder.h"
+
+void MakeBinFromFile(std::string input_file, std::string output_file_path, int64_t mask){
+    std::string str;
+    std::ifstream file(input_file, std::ios_base::binary);
+    std::ofstream outfile(output_file_path, std::ios_base::binary);
+    int64_t res;
+    if(file.is_open()){
+        while(file.read((char*) &res, sizeof(int64_t))){
+            res ^= mask;
+            outfile.write((char*) &res, sizeof(res));
+        }
+        file.close();
+        outfile.close();
+    } else {
+        return;
+    }
+
+}
+
+void Delay(int n){
+    QEventLoop loop;
+    QTimer::singleShot(n, &loop, SLOT(quit()));
+    loop.exec();
+}
---
+++ coder.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+#include <fstream>
+#include <QTimer>
+#include <QEventLoop>
+
+void Delay(int n);
+
+void MakeBinFromFile(std::string input_file, std::string output_file_path, int64_t mask);
